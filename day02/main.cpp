9e0d27e8865da499df0fd6239ee709f855e07827
Andreas Magnusson <a.magnusson@tele-radio.com>
2019-01-21 08:49:44 +0100

    solved day2 part two

--- day02/main.cpp
+++ day02/main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <fstream>
+#include <map>
 
 using namespace std;
 
 
-void readFile(int *buffer, string& filename)
+void readFile(string *buffer, string& filename)
 {
     fstream inputFile;
-    int x, i;
+    string x;
+    int i;
     inputFile.open(filename);
     i = 0;
     while (inputFile >> x) {
@@ -19,41 +21,82 @@ void readFile(int *buffer, string& filename)
 
 int main()
 {
+    const int dataSize = 250;
+    const int stringSize = 26;
     fstream inputFile;
     string filename;
-    int array[1038];
-    int frequencies[1038*1038];
-    int i, currentFreq, j, num, notfound;
+    string array[dataSize];
+    map<char, int> letterMap;
+    map<char, int>::iterator it;
+    int i, j, k, count;
+    bool foundTwo, foundThree;
+    char c;
+    int twoLetterCount, threeLetterCount;
 
     filename = "input.txt";
     readFile(array, filename);
 
     // Part One
-    currentFreq = 0;
-    for (i = 0; i < 1038; i++) {
-        currentFreq += array[i];
+    twoLetterCount = 0; threeLetterCount = 0;
+    for (i = 0; i < dataSize; i++) {
+        for (j = 0; j < array[i].size(); j++) {
+            c = array[i][j];
+            if (letterMap.find(c) == letterMap.end()) {
+                letterMap[c] = 1;
+            } else {
+                letterMap[c]++;
+            }
+        }
+        it = letterMap.begin();
+        foundTwo = false; foundThree = false;
+        while (it != letterMap.end()) {
+            count = it->second;
+            if (count == 2 && !foundTwo) {
+                twoLetterCount++;
+                foundTwo = true;
+            } else if (count == 3 && !foundThree) {
+                threeLetterCount++;
+                foundThree = true;
+            }
+            it++;
+        }
+        letterMap.clear();
     }
-    printf("Resulting frequency: %d\n", currentFreq);
+    printf("My checksum: %d\n", twoLetterCount * threeLetterCount);
 
     // Part Two
-    currentFreq = 0;
-    num = 0;
+    bool notMatching;
+    bool done = false;
+    int result1, result2, resultk;
     i = 0;
-    notfound = 1;
-    while (notfound) {
-        currentFreq += array[i];
-        for (j = 0; j < num; j++) {
-            if (currentFreq == frequencies[j]) {
-                notfound = 0;
+    while (!done) {
+        for (j = i + 1; j < dataSize; j++) {
+            if (done) {
+                break;
             } else {
-                frequencies[num] = currentFreq;
+                notMatching = false;
+                for (k = 0; k < stringSize; k++) {
+                    if (array[i][k] != array[j][k]) {
+                        if (notMatching) {
+                            break;
+                        } else {
+                            notMatching = true;
+                            resultk = k;
+                        }
+                    } else if (k == stringSize - 1 && notMatching) {
+                        done = true;
+                        result1 = i;
+                        result2 = j;
+                    }
+                }
             }
         }
         i++;
-        i %= 1038;
-        num++;
     }
-    printf("First frequency reached twice: %d\n", currentFreq);
-
+    printf("%d, %d, %d\n", result1, result2, resultk);
+    printf("%s\n", array[result1].c_str());
+    printf("%s\n", array[result2].c_str());
+    array[result1].erase(resultk, 1);
+    printf("%s\n", array[result1].c_str());
 
 }
