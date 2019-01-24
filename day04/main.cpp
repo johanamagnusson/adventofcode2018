8bad3108c011d20e301857f969b11857a9b2daf7
Andreas Magnusson <a.magnusson@tele-radio.com>
2019-01-24 17:00:37 +0100

    added Entry class and readFile to day04

--- day04/main.cpp
+++ day04/main.cpp
@@ -1,77 +1,42 @@
 #include <iostream>
 #include <fstream>
 #include <regex>
-#include <string>
-#include <cstdlib>
-#include <map>
 
 using namespace std;
 
 
 
 
-class Claim
-{
-    public:
-        int id, x, y, w, h;
-        Claim();
-        //friend bool operator<(const Claim &c1, const Claim &c2);
-};
-Claim::Claim()
-{
-    id = 0; x = 0; y = 0; w = 0; h = 0;
-}
-bool operator<(Claim c1, Claim c2)
-{
-    return c1.id != c2.id;
-}
-
-bool overlap(Claim c1, Claim c2)
-{
-    if (c1.x >= c2.x + c2.w || c2.x >= c1.x + c1.w) {
-        return false;
-    }
-
-    if (c1.y >= c2.y + c2.h || c2.y >= c1.y + c1.h) {
-        return false;
-    }
-
-    return true;
-}
-
-class Square
+class Entry
 {
     private:
-        int x, y;
+        string entryString;
 
     public:
-        int matrix[1000][1000];
-        Square();
-        void updateCount(Claim);
+        int year, month, day, min, sec;
+
+        Entry();
+        void setEntryString(string newEntryString);
+        string getEntryString();
 };
-Square::Square()
-{
-    x = 0; y = 0;
-    int i, j;
-    for (i = 0; i < 1000; i++) {
-        for (j = 0; j < 1000; j++) {
-            matrix[i][j] = 0;
-        }
-    }
 
-}
-void Square::updateCount(Claim c)
+Entry::Entry()
 {
-    int i, j;
-    for (i = c.x; i < c.x + c.w; i++) {
-        for (j = c.y; j < c.y + c.h; j++) {
-            matrix[i][j]++;
-        }
-    }
+    year = 0; month = 0; day = 0; min = 0; sec = 0;
+}
+
+void Entry::setEntryString(string newEntryString)
+{
+    entryString = newEntryString;
+}
+
+string Entry::getEntryString()
+{
+    return entryString;
 }
 
 
-void readFile(Claim *buffer, string& filename)
+void readFile(Entry *buffer, string& filename)
 {
     fstream inputFile;
     smatch results;
@@ -81,12 +46,13 @@ void readFile(Claim *buffer, string& filename)
     inputFile.open(filename);
     i = 0;
     while (getline(inputFile, c)) {
-        found = regex_match(c, results, regex("#(\\d+) @ (\\d+),(\\d+): (\\d+)x(\\d+)"));
-        buffer[i].id = stoi(results[1].str());
-        buffer[i].x  = stoi(results[2].str());
-        buffer[i].y  = stoi(results[3].str());
-        buffer[i].w  = stoi(results[4].str());
-        buffer[i].h  = stoi(results[5].str());
+        buffer[i].setEntryString(c);
+        found = regex_match(c, results, regex("\\[(\\d+)-(\\d+)-(\\d+) (\\d+):(\\d+)\\] .*$"));
+        buffer[i].year = stoi(results[1].str());
+        buffer[i].month = stoi(results[2].str());
+        buffer[i].day = stoi(results[3].str());
+        buffer[i].min = stoi(results[4].str());
+        buffer[i].sec = stoi(results[5].str());
         i++;
     }
     inputFile.close();
@@ -98,53 +64,13 @@ void readFile(Claim *buffer, string& filename)
 
 int main()
 {
-    const int numClaims = 1367;
-    const int squareSize = 1000;
+    const int numEntries = 1186;
     string filename;
-    Claim array[numClaims];
-    Square square;
-    int i, j, count;
+    Entry array[numEntries];
 
     filename = "input.txt";
     readFile(array, filename);
 
-    // Part One
-    for (i = 0; i < numClaims; i++) {
-        square.updateCount(array[i]);
-    }
-
-    count = 0;
-    for (i = 0; i < squareSize; i++) {
-        for (j = 0; j < squareSize; j++) {
-            if (square.matrix[i][j] >= 2) {
-                count++;
-            }
-        }
-    }
-    printf("%d\n", count);
-
-
-    // Part Two
-    Claim result;
-    bool done;
-    done = false;
-    for (i = 0; i < numClaims; i++) {
-        for (j = 0; j < numClaims; j++) {
-            if (i == j) {
-                continue;
-            } else if (overlap(array[i], array[j])) {
-                break;
-            } else if (j == numClaims - 1) {
-                result = array[i];
-                done = true;
-            }
-        }
-        if (done) {
-            break;
-        }
-    }
-
-    printf("%d\n", result.id);
 
 
 }
