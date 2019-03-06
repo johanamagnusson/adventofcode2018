0b53917ddaad4ee84ad9d163bc4c6cd18e533116
Andreas Magnusson <andreas.magnusson90@gmail.com>
2019-03-06 16:16:37 +0100

    solved day06

--- day06/Main.java
+++ day06/Main.java
@@ -3,20 +3,50 @@ import java.util.regex.*;
 import java.lang.*;
 import java.io.*;
 
+class DistanceToNode implements Comparable<DistanceToNode>
+{
+    int id, value;
 
+    public DistanceToNode(int id, int value) {
+        this.id = id;
+        this.value = value;
+    }
+
+    public int compareTo(DistanceToNode d) {
+        return this.value - d.value;
+    }
+}
+
+class Grid
+{
+    int[][] grid;
+    int xMin, xMax, yMin, yMax;
+
+    public Grid(int xMin, int xMax, int yMin, int yMax) {
+        this.xMin = xMin;
+        this.xMax = xMax;
+        this.yMin = yMin;
+        this.yMax = yMax;
+        this.grid = new int[xMax][yMax];
+    }
+
+    public void setPoint(int x, int y, int id) {
+        grid[x - this.xMin][y - this.yMin] = id;
+    }
+}
 
 public class Main
 {
-    static final int DATA_SIZE = 7;
+    static final int DATA_SIZE = 50;
 
     public static int[][] readFile(String fileName) {
-        int[][] input = new int[DATA_SIZE + 1][2];
+        int[][] input = new int[DATA_SIZE + 2][2];
         try {
             File file = new File(fileName);
             Scanner scanner = new Scanner(file);
             String str;
             String[] xy;
-            int i = 0, maxX = 0, maxY = 0;
+            int i = 0, maxX = 0, maxY = 0, minX = 1000, minY = 1000;
             while (scanner.hasNext()) {
                 str = scanner.nextLine();
                 xy = str.split(", ");
@@ -24,10 +54,14 @@ public class Main
                 input[i][1] = Integer.parseInt(xy[1]);
                 if (input[i][0] > maxX) maxX = input[i][0];
                 if (input[i][1] > maxY) maxY = input[i][1];
+                if (input[i][0] < minX) minX = input[i][0];
+                if (input[i][1] < minY) minY = input[i][1];
                 i++;
             }
             input[DATA_SIZE][0] = maxX;
             input[DATA_SIZE][1] = maxY;
+            input[DATA_SIZE + 1][0] = minX;
+            input[DATA_SIZE + 1][1] = minY;
             scanner.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -35,33 +69,69 @@ public class Main
         return input;
     }
 
-    public static void printGrid(int[][] grid) {
-        for (int i = 0; i < grid.length; i++) {
-            System.out.println(Arrays.toString(grid[i]));
-        }
+    public static int manhattanDistance(int x1, int y1, int x2, int y2) {
+        return Math.abs(x1 - x2) + Math.abs(y1 - y2);
     }
 
     public static void main(String []args) {
-        String fileName = "testInput.txt";
+        String fileName = "input.txt";
         int[][] input;
         input = readFile(fileName);
         int maxX = input[DATA_SIZE][0]; int maxY = input[DATA_SIZE][1];
+        int minX = input[DATA_SIZE + 1][0]; int minY = input[DATA_SIZE + 1][1];
         int numberCoords = maxX * maxY;
+        int infinity = numberCoords;
+        Grid grid = new Grid(minX, maxX, minY, maxY);
+        int[] nodeHistogram = new int[DATA_SIZE];
+        ArrayList<DistanceToNode> distances = new ArrayList<DistanceToNode>();
 
-        int[][] grid = new int[maxY + 1][maxX + 1];
-
-        for (int i = 0; i < DATA_SIZE; i++) {
-            grid[input[i][1]][input[i][0]] = i + 1;
+        for (int x = minX; x < maxX; x++) {
+            for (int y = minY; y < maxY; y++) {
+                for (int i = 0; i < DATA_SIZE; i++) {
+                    int distance = manhattanDistance(x, y, input[i][0], input[i][1]);
+                    distances.add(new DistanceToNode(i, distance));
+                }
+                Collections.sort(distances);
+                if (distances.get(0).value == distances.get(1).value) {
+                    grid.setPoint(x, y, -1);
+                } else {
+                    grid.setPoint(x, y, distances.get(0).id);
+                    if (x == minX || x == maxX || y == minY || y == maxY) {
+                        nodeHistogram[distances.get(0).id] = infinity;
+                    } else {
+                        nodeHistogram[distances.get(0).id]++;
+                    }
+                }
+                distances.clear();
+            }
         }
 
-        printGrid(grid);
+        //System.out.println(Arrays.toString(nodeHistogram));
 
-        /*
-        int checked = 0;
-        int r = 1;
-        while (checked < numberCoords) {
-            for (i = 0; i < 50; i++) {
-          */      
+        int maxArea = 0;
+        for (int i = 0; i < DATA_SIZE; i++) {
+            if ((nodeHistogram[i] > maxArea) && !(nodeHistogram[i] >= infinity)) {
+                maxArea = nodeHistogram[i];
+            }
+        }
+
+        System.out.println(String.format("Part 1 answer: %d", maxArea));
+
+        int totalDistance;
+        int regionSize = 0;
+        for (int x = minX; x < maxX; x++) {
+            for (int y = minY; y < maxY; y++) {
+                totalDistance = 0;
+                for (int i = 0; i < DATA_SIZE; i++) {
+                    totalDistance += manhattanDistance(x, y, input[i][0], input[i][1]);
+                }
+                if (totalDistance < 10000) {
+                    regionSize++;
+                }
+            }
+        }
+
+        System.out.println(String.format("Part 2 answer: %d", regionSize));
 
     }
 }
